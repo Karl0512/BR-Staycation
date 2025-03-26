84fde6f645d916994fe33005a0f9f7e855c27320
Bhrenz <btbanaag0213ant@student.fatima.edu.ph>
2025-03-26 16:02:39 +0800

    testing

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false, // Disable logging (optional)
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Required for cloud databases
+    },
+  },
 });
 
 async function authenticateDB() {
