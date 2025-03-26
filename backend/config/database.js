ea47d11762ceca6be8e61b7f2c8947ca763e0482
Bhrenz <btbanaag0213ant@student.fatima.edu.ph>
2025-03-26 16:00:22 +0800

    trying to deploy

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,21 +1,17 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize({
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  host: process.env.DB_HOST, // Example: 'localhost'
-  username: process.env.DB_USER, // Example: 'user'
-  password: process.env.DB_PASSWORD, // Example: 'password'
-  database: process.env.DB_NAME, // Example: 'BrStaycation'
-  port: process.env.DB_PORT, // Default PostgreSQL port
+  logging: false, // Disable logging (optional)
 });
 
 async function authenticateDB() {
   try {
     await sequelize.authenticate();
-    console.log('Connection to the database has been established successfully.');
+    console.log('✅ Database connection successful!');
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error('❌ Unable to connect to the database:', error);
   }
 }
 
