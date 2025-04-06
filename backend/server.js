5ae5a2669f8f1c99cf8aa81d21e99949f7615dc6
Bhrenz Cahrlwheng Banaag <btbanaag0213ant@student.fatima.edu.ph>
2025-04-06 14:26:01 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Allow all origins for testing purposes
+  origin: '', // Allow all origins for testing purposes
   credentials: true,
   methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Allow necessary HTTP methods
 }));
