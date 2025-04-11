a9ac74c98dff8691db76371bf87dfeb9220e6d2f
Bhrenz Cahrlwheng Banaag <btbanaag0213ant@student.fatima.edu.ph>
2025-04-11 16:57:18 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://br-staycation-frontend.onrender.com', // Allow all origins for testing purposes
+  origin: true, // Allow all origins for testing purposes
   credentials: true,
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'], // Allow necessary HTTP methods
 }));
